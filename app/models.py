708b6170da3d8d1362247810a0e1d173a992047e
Mohamedlee27 <leonk8070@gmail.com>
2022-05-18 10:03:23 +0300

    first commit

--- app/models.py
+++ app/models.py
@@ -6,6 +6,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(10), index=True, unique=True)
     email = db.Column(db.String(64), index=True, unique=True)
-
+    password = db.Column()
     def __repr__(self):
         return '<User {}>'.format(self.username)
